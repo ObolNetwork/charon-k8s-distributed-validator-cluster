0cfa398b04b15b0d64d251ac34d500047d980548
Aly <16789036+eth2devops@users.noreply.github.com>
2022-10-18 07:21:45 -0400

    support secrets recreation

--- create-secrets.sh
+++ create-secrets.sh
@@ -19,7 +19,7 @@ fi
 # set current namespace
 kubectl config set-context --current --namespace=$ns
 
-kubectl -n $ns create secret generic cluster-lock --from-file=cluster-lock.json=./.charon/cluster/cluster-lock.json
+kubectl -n $ns create secret generic cluster-lock --from-file=cluster-lock.json=./.charon/cluster/cluster-lock.json --dry-run=client -o yaml | kubectl apply -f -
 
 i=0
 while [[ $i -lt "$CLUSTER_SIZE" ]]
@@ -28,8 +28,8 @@ do
     for secret in ./.charon/cluster/node${i}/validator_keys/*; do
         files="$files --from-file=./.charon/cluster/node${i}/validator_keys/$(basename $secret)"
     done
-    kubectl -n $ns create secret generic node${i}-validators $files
-    kubectl -n $ns create secret generic node${i}-charon-enr-private-key --from-file=charon-enr-private-key=./.charon/cluster/node${i}/charon-enr-private-key
-    kubectl -n $ns create secret generic node${i}-cluster-lock --from-file=cluster-lock.json=./.charon/cluster/cluster-lock.json
+    kubectl -n $ns create secret generic node${i}-validators $files --dry-run=client -o yaml | kubectl apply -f -
+    kubectl -n $ns create secret generic node${i}-charon-enr-private-key --from-file=charon-enr-private-key=./.charon/cluster/node${i}/charon-enr-private-key --dry-run=client -o yaml | kubectl apply -f -
+    kubectl -n $ns create secret generic node${i}-cluster-lock --from-file=cluster-lock.json=./.charon/cluster/cluster-lock.json --dry-run=client -o yaml | kubectl apply -f -
     ((i=i+1))
 done
