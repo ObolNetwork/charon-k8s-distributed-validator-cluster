cce0f2d15e08c2e6e02a99882f686cb4bb8c0424
Sukhendu Ghosh <sukhendu@obol.tech>
2023-06-26 19:59:50 +0530

    deploy charon with the image sha sent by github event

---
+++ deploy-cluster-with-charon-tag.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+if [ "$1" = "" ]
+then
+  echo "Usage: $0 <cluster name to be deployed>"
+  exit -1
+fi
+
+if [ "$2" = "" ]
+then
+  echo "Usage: $0 <charon image tag to deploy>"
+  exit -1
+fi
+
+set -uo pipefail
+
+CLUSTER_NAME=$1
+CHARON_IMAGE_TAG=$2
+
+# download cluster config
+gcloud storage cp gs://charon-clusters-config/${CLUSTER_NAME}/${CLUSTER_NAME}.env .
+
+# override the env vars
+OLDIFS=$IFS
+IFS='
+'
+export $(< ./${CLUSTER_NAME}.env)
+IFS=$OLDIFS
+
+rm ./${CLUSTER_NAME}.env
+
+# create the namespace
+nsStatus=`kubectl get namespace ${CLUSTER_NAME} --no-headers --output=go-template={{.metadata.name}} 2>/dev/null`
+if [ -z "$nsStatus" ]; then
+    echo "Cluster (${CLUSTER_NAME}) not found, creating a new one."
+    kubectl create namespace ${CLUSTER_NAME} --dry-run=client -o yaml | kubectl apply -f -
+fi
+
+# set current namespace
+kubectl config set-context --current --namespace=${CLUSTER_NAME}
+
+echo "deploying cluster: ${CLUSTER_NAME}"
+
+# Deploy charon nodes
+IFS=','
+read -a versions <<< "$CHARON_VERSIONS"
+node_index=0
+for version in "${versions[@]}"
+do
+export NODE_NAME="node$node_index"
+export VC_INDEX="vc$node_index"
+if [ "$version" = "latest" ]; then
+    export CHARON_VERSION="${CHARON_IMAGE_TAG}"
+else
+    export CHARON_VERSION="$version"
+fi
+eval "cat <<EOF
+$(<./templates/charon.yaml)
+EOF
+" | kubectl apply -f -
+((node_index=node_index+1))
+done
+
+# Deploy Validator client of required type for each charon node. 
+IFS=','
+read -a vcs <<< "$VC_TYPES"
+node_index=0
+for vc in "${vcs[@]}"
+do
+export NODE_NAME="node$node_index"
+export VC_INDEX="vc$node_index"
+if [ $vc -eq 0 ]; then
+eval "cat <<EOF
+$(<./templates/teku-vc.yaml)
+EOF
+" | kubectl apply -f -
+elif [ $vc -eq 1 ]; then
+eval "cat <<EOF
+$(<./templates/lighthouse-vc.yaml)
+EOF
+" | kubectl apply -f -
+elif [ $vc -eq 2 ]; then
+eval "cat <<EOF
+$(<./templates/lodestar-vc.yaml)
+EOF
+" | kubectl apply -f -
+fi
+((node_index=node_index+1))
+done
+
+# deploy prometheus agent
+export CLUSTER_NAME="${CLUSTER_NAME}"
+export MONITORING_TOKEN="$MONITORING_TOKEN"
+eval "cat <<EOF
+$(<./templates/prom-agent.yaml)
+EOF
+" | kubectl apply -f -
