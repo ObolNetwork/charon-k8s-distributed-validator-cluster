cfdfc66f54ea2cae6ea8786543e9a7a1042e131b
Sukhendu Ghosh <sukhendu@obol.tech>
2023-07-04 18:52:27 +0530

    Add dkg test for testing relays

---
+++ create-cluster-secrets-dkg.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -uo pipefail
+
+if [ "$1" = "" ]
+then
+  echo "Usage: $0 <cluster name to be deployed>"
+  exit
+fi
+
+CLUSTER_NAME=$1
+
+# download cluster config
+gcloud storage cp gs://charon-clusters-config/${CLUSTER_NAME}/${CLUSTER_NAME}.env .
+
+# override the env vars
+OLDIFS=$IFS
+IFS='
+'
+export $(< ./${CLUSTER_NAME}.env)
+IFS=$OLDIFS
+
+# delete cluster env vars file
+rm ./${CLUSTER_NAME}.env
+
+# create the namespace
+nsStatus=$(kubectl get namespace ${CLUSTER_NAME} --no-headers --output=go-template={{.metadata.name}} 2>/dev/null)
+if [ -z "$nsStatus" ]; then
+    echo "Cluster (${CLUSTER_NAME}) not found, creating a new one."
+    kubectl create namespace ${CLUSTER_NAME} --dry-run=client -o yaml | kubectl apply -f -
+fi
+
+# download cluster config
+mkdir -p ./.charon
+gcloud storage cp -r gs://charon-clusters-config/${CLUSTER_NAME} ./.charon/
+
+# set current namespace
+kubectl config set-context --current --namespace=${CLUSTER_NAME}
+
+i=0
+while [[ $i -lt "$NODES" ]]
+do
+    kubectl -n ${CLUSTER_NAME} create secret generic node${i}-charon-enr-private-key --from-file=charon-enr-private-key=./.charon/${CLUSTER_NAME}/node${i}/charon-enr-private-key --dry-run=client -o yaml | kubectl apply -f -
+    kubectl -n ${CLUSTER_NAME} create secret generic node${i}-cluster-definition --from-file=cluster-definition.json=./.charon/${CLUSTER_NAME}/cluster-definition.json --dry-run=client -o yaml | kubectl apply -f -
+    ((i=i+1))
+    #exit loop after first iteration
+    # break
+done
+
+# delete cluster config before exit
+rm -rf ./.charon/${CLUSTER_NAME}
