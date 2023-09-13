b4d349aa9dc02d25a74a5846d7981b27f850b93d
Sukhendu Ghosh <sukhendu@obol.tech>
2023-09-13 16:48:32 +0200

    Complete sluster setup utils with web3signer

--- create-web3signer-secrets.sh
+++ create-web3signer-secrets.sh
@@ -10,6 +10,14 @@ fi
 
 CLUSTER_NAME=$1
 
+# override the env vars
+OLDIFS=$IFS
+IFS='
+'
+export $(< ./${CLUSTER_NAME}.env)
+IFS=$OLDIFS
+
+
 # create the namespace
 nsStatus=$(kubectl get namespace ${CLUSTER_NAME} --no-headers --output=go-template={{.metadata.name}} 2>/dev/null)
 if [ -z "$nsStatus" ]; then
@@ -17,6 +25,20 @@ if [ -z "$nsStatus" ]; then
     kubectl create namespace ${CLUSTER_NAME} --dry-run=client -o yaml | kubectl apply -f -
 fi
 
+# download cluster config
+mkdir -p ./.charon
+gcloud storage cp -r gs://charon-clusters-config/${CLUSTER_NAME} ./.charon/
+
 # set current namespace
 kubectl config set-context --current --namespace=${CLUSTER_NAME}
-kubectl -n ${CLUSTER_NAME} create secret generic keystore --from-file=keystore.yaml=./utils/go-utils/web3signerHex/keystore.yaml --dry-run=client -o yaml | kubectl apply -f -
+
+i=0
+while [[ $i -lt "$NODES" ]]
+do
+    kubectl -n ${CLUSTER_NAME} create secret generic node${i}-web3signer-keystore --from-file=keystore.yaml=./utils/go-utils/web3signerHex/${CLUSTER_NAME}-node${i}.yaml --dry-run=client -o yaml | kubectl apply -f -
+    ((i=i+1))
+done
+
+# delete cluster config before exit
+rm -rf ./.charon/${CLUSTER_NAME}
+
