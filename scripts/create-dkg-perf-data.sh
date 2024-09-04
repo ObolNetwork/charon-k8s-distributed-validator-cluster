5690a78145beceeced8201c2457357afebcba415
st1p3 <stipe@obol.tech>
2024-09-04 11:37:42 +0200

    change binbash line

--- scripts/create-dkg-perf-data.sh
+++ scripts/create-dkg-perf-data.sh
@@ -1,9 +1,9 @@
-
 #!/bin/bash
+
 # set -uo pipefail
 CLUSTER_NAME_PREFIX="relay-dkg-perf"
 
-COPY_FROM_CLUSTER_NAME="charon-dkg-test"
+# COPY_FROM_CLUSTER_NAME="charon-dkg-test"
 
 # override the env vars
 OLDIFS=$IFS
@@ -13,7 +13,7 @@ export $(< ./envs/charon-dkg-test.env)
 IFS=$OLDIFS
 
 INDEX=0
-for ((i = 1; i <= 10; i++)); do
+for ((i = 1; i <= ${CLUSTERS}; i++)); do
 docker run -it --rm -v "$PWD:/opt/charon" obolnetwork/charon:${CHARON_VERSION} create cluster --fee-recipient-addresses="0xBc7c960C1097ef1Af0FD32407701465f3c03e407" --nodes=${NODES} --network=${NETWORK} --withdrawal-addresses="0xBc7c960C1097ef1Af0FD32407701465f3c03e407" --name=test --num-validators=${NUM_VALIDATORS}
 # Set the path to the cluster-lock.json file
 cluster_lock_file="node0/cluster-lock.json"
