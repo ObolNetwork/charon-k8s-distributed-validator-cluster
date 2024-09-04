0ad9cae15b44e52e9e222ee6b0247eb599a45a86
st1p3 <stipe@obol.tech>
2024-09-04 11:52:05 +0200

    remove it

--- scripts/create-dkg-perf-data.sh
+++ scripts/create-dkg-perf-data.sh
@@ -14,7 +14,8 @@ IFS=$OLDIFS
 
 INDEX=0
 for ((i = 1; i <= ${CLUSTERS}; i++)); do
-docker run -it --rm -v "$PWD:/opt/charon" obolnetwork/charon:${CHARON_VERSION} create cluster --fee-recipient-addresses="0xBc7c960C1097ef1Af0FD32407701465f3c03e407" --nodes=${NODES} --network=${NETWORK} --withdrawal-addresses="0xBc7c960C1097ef1Af0FD32407701465f3c03e407" --name=test --num-validators=${NUM_VALIDATORS}
+docker run --rm -v "$PWD:/opt/charon" obolnetwork/charon:${CHARON_VERSION} create cluster --fee-recipient-addresses="0xBc7c960C1097ef1Af0FD32407701465f3c03e407" --nodes=${NODES} --network=${NETWORK} --withdrawal-addresses="0xBc7c960C1097ef1Af0FD32407701465f3c03e407" --name=test --num-validators=${NUM_VALIDATORS}
+ls -la node0
 # Set the path to the cluster-lock.json file
 cluster_lock_file="node0/cluster-lock.json"
 
